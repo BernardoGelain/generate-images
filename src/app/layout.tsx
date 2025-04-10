402b4185bbec4071b7203875fc45fb38e935b892
Bernardo Gelain Dariva <bernardogelaindariva@MacBook-Pro-de-Bernardo.local>
2025-04-09 22:24:28 -0300

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
 import "./globals.css";
 import AudioPlayer from "../components/AudioPlayer/AudioPlayer";
 import { MusicProvider } from "@/contexts/MusicContext";
-
-const geistSans = Montserrat({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Montserrat({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Refuge from Your Absence",
--- src/app/moments/page.tsx
+++ src/app/moments/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { TextField, Button, CircularProgress, Container, Typography } from "@mui/material";
+import { TextField, CircularProgress, Container } from "@mui/material";
 import PhotoCarousel from "../../components/PhotoCarousel/PhotoCarousel";
 import { useRouter } from "next/navigation";
 
